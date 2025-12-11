0a64b90d84b835db4fce1d60cc4f901bf776af09
kiruthigagokul <kiruthigasarasulingam@gmail.com>
2025-12-11 14:04:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ resource "aws_instance" "my_ec2_vm" {
   ami           = "ami-027f6edb9cfe9ca87"  # Amazon Linux 2023 (ap-south-1)
   instance_type = "t3.small"
   availability_zone="ap-south-1a"
-  subnet_id = aws_subnet.public1.id
+  subnet_id = "subnet-000d338018d5822e3"
 
   tags = {
     Name = "web"
   }
 }
+
 
 
 
