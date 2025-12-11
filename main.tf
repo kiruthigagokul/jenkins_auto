515b5206159a6f595d5587fa9d49adfb73d0ec06
kiruthigagokul <kiruthigasarasulingam@gmail.com>
2025-12-11 13:56:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,13 @@
 
 resource "aws_instance" "my_ec2_vm" {
   ami           = "ami-027f6edb9cfe9ca87"  # Amazon Linux 2023 (ap-south-1)
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   availability_zone="ap-south-1a"
   tags = {
     Name = "web"
   }
 }
+
 
 
 
